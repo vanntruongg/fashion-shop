f98e847bba1787544f3785c95e23c7b23f08d5b0
Van Truong <truongtran.lv2019@gmail.com>
2023-09-26 16:12:28 +0700

    add checkout page

---
+++ resources/views/pages/guest/checkout.blade.php
@@ -0,0 +1,9 @@
+@component('components.head', ['title' => 'Thanh toán'])
+@endcomponent
+
+<main class="max-w-[1360px] mx-auto px-16 my-10 mt-[137px]">
+  <h2>Thanh toán</h2>
+</main>
+
+
+<x-footer :js-file="''" />
